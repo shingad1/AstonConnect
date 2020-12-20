cdf8b85ee448df68091453513dbef6809462131b
Devin S <devin_shingadia11@hotmail.com>
2020-12-20 11:35:11 +0000

    post modal class

--- app/src/main/java/com/devin/astonconnect/Model/Post.java
+++ app/src/main/java/com/devin/astonconnect/Model/Post.java
@@ -1,12 +1,27 @@
 package com.devin.astonconnect.Model;
 
 public class Post {
-    private String  postid;
     private String  description;
-    private String  postImage;
+    private Boolean isimagepost;
+    private String  postid;
+    private String  postimage;
     private String  publisher;
     private String  title;
-    private Boolean isImagePost;
+
+    public String  getdescription() { return description; }
+    public Boolean getisimagepost() { return isimagepost; }
+    public String  gettitle()       { return title; }
+    public String  getpostid()      { return postid; }
+    public String  getpostimage()   { return postimage; }
+    public String  getpublisher()   { return publisher; }
+
+
+    public void setpublisher(String publisher)      { this.publisher = publisher; }
+    public void setpostimage(String postimage)      { this.postimage = postimage; }
+    public void setisimagepost(Boolean isimagepost) { this.isimagepost = isimagepost; }
+    public void settitle(String title)              { this.title = title; }
+    public void setdescription(String description)  { this.description = description; }
+    public void setpostid(String postid)            { this.postid = postid; }
 
 
 }
