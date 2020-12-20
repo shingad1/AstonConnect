b967a0a4e1b3db989674aedb36a81f63bb756c4b
Devin S <devin_shingadia11@hotmail.com>
2020-12-20 13:27:51 +0000

    Retrieve posts based on the users you follow

--- app/src/main/java/com/devin/astonconnect/Adapter/PostAdapter.java
+++ app/src/main/java/com/devin/astonconnect/Adapter/PostAdapter.java
@@ -1,5 +1,131 @@
 package com.devin.astonconnect.Adapter;
 
-public class PostAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.devin.astonconnect.Model.Post;
+import com.devin.astonconnect.Model.User;
+import com.devin.astonconnect.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
+
+    public  Context mContext;
+    public  List<Post> mPost;
+    private FirebaseUser firebaseUser;
+
+    public PostAdapter(Context mContext, List<Post> mPost) {
+        this.mContext = mContext;
+        this.mPost = mPost;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.post_item, parent, false);
+        return new PostAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Post post = mPost.get(position);
+
+        holder.title.setText(post.gettitle());
+        holder.isImagePost = post.getisimagepost();
+
+        /** Change the visibility of the post (viewholder) based on values **/
+        if(post.getisimagepost() == false){
+            holder.post_image.setVisibility(View.GONE);
+            holder.bottomDescription.setVisibility(View.GONE);
+            holder.mainDescription.setVisibility(View.VISIBLE);
+            holder.mainDescription.setText(post.getdescription());
+        } else {
+            holder.post_image.setVisibility(View.VISIBLE);
+            holder.bottomDescription.setVisibility(View.VISIBLE);
+            Glide.with(mContext).load(post.getpostimage()).into(holder.post_image);
+            holder.bottomDescription.setText(post.getdescription());
+        }
+
+        if(post.getdescription() == ""){
+            holder.bottomDescription.setVisibility(View.GONE);
+        } else {
+            holder.bottomDescription.setVisibility(View.VISIBLE);
+            holder.bottomDescription.setText(post.getdescription());
+        }
+
+        getPublisherInfo(holder.profile_image, holder.fullname, holder.publisher, post.getpublisher());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+
+    private void getPublisherInfo(ImageView profile_image,  TextView fullname, TextView publisher, String userid){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(userid); //Get the database location based on the user id
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                Glide.with(mContext).load(user.getimageurl()).into(profile_image);
+                fullname.setText(user.getfullname());
+                publisher.setText(user.getusername());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView profile_image, post_image, like, comment, bookmark;
+        public TextView  title, mainDescription, likeText, username, fullname, publisher, bottomDescription, comments;
+        public Boolean isImagePost;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profile_image = itemView.findViewById(R.id.profile_image);
+            post_image    = itemView.findViewById(R.id.post_image);
+            like          = itemView.findViewById(R.id.like);
+            comment       = itemView.findViewById(R.id.comment);
+            bookmark      = itemView.findViewById(R.id.bookmark);
+
+            //Textview
+            title               = itemView.findViewById(R.id.title);
+            mainDescription     = itemView.findViewById(R.id.mainDescription);
+            likeText            = itemView.findViewById(R.id.likeText);
+            fullname            = itemView.findViewById(R.id.fullname);
+            publisher           = itemView.findViewById(R.id.publisher);
+            bottomDescription   = itemView.findViewById(R.id.bottomDescription);
+            comments            = itemView.findViewById(R.id.comments);
+        }
+    }
 
 }
--- app/src/main/java/com/devin/astonconnect/NewsfeedFragment.java
+++ app/src/main/java/com/devin/astonconnect/NewsfeedFragment.java
@@ -3,22 +3,48 @@ package com.devin.astonconnect;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.devin.astonconnect.Adapter.PostAdapter;
 import com.devin.astonconnect.Loading.LoadingDialog;
+import com.devin.astonconnect.LoginRegister.StartActivity;
+import com.devin.astonconnect.Model.Post;
 import com.devin.astonconnect.Post.ReviewImagePostActivity;
 import com.devin.astonconnect.Post.ReviewTextPostActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsfeedFragment extends Fragment {
 
+    //Posts stuff
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> mPosts;
+    private List<String> followingList;
+    private ImageView logOutBtn;
+
+    //Floating Action button stuff
     private Animation rotateOpen, rotateClose, fromBottom, toBottom;
     private FloatingActionButton add_btn, createTextPostBtn, createImagePostBtn;
     private Boolean clicked = false; //for animation purposes
@@ -29,9 +55,34 @@ public class NewsfeedFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_newsfeed, container, false);
 
+        /** Displaying posts stuff **/
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        mPosts = new ArrayList<>();
+        followingList = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(), mPosts);
+        recyclerView.setAdapter(postAdapter);
+
+        /** Floating action button stuff **/
         add_btn            = view.findViewById(R.id.add_btn);
         createTextPostBtn = view.findViewById(R.id.createPostBtn);
         createImagePostBtn = view.findViewById(R.id.createImagePostBtn);
+
+        //logout btn
+        logOutBtn = view.findViewById(R.id.logOutBtn);
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), StartActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
 
         //Animations for the buttons
         rotateOpen  = AnimationUtils.loadAnimation((getActivity()), R.anim.rotate_open_anim);
@@ -62,8 +113,62 @@ public class NewsfeedFragment extends Fragment {
             }
         });
 
+        checkFollowing();
         return view;
     }
+
+
+    private void checkFollowing(){
+        followingList = new ArrayList<>();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Follow")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child("following");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                followingList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    followingList.add(snapshot.getKey());
+                }
+                readPosts();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void readPosts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mPosts.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    for(String id : followingList){
+                        if(post.getpublisher().equals(id)){
+                            mPosts.add(post);
+                        }
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
 
     private void onAddButtonClicked() {
         setVisibility(clicked);
