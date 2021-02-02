15931d3359043cd519534342667285aefce042d8
Devin S <36890301+shingad1@users.noreply.github.com>
2021-02-02 18:13:07 +0000

    MessageActivity

---
+++ app/src/main/java/com/devin/astonconnect/Chat/MessageActivity.java
@@ -0,0 +1,16 @@
+package com.devin.astonconnect.Chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devin.astonconnect.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
