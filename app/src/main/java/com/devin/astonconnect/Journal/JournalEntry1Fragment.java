894cb86dccf2e2795f79b221e6dbc74eb64f8598
Devin S <36890301+shingad1@users.noreply.github.com>
2021-02-20 20:58:38 +0000

    add fields

--- app/src/main/java/com/devin/astonconnect/Journal/JournalEntry1Fragment.java
+++ app/src/main/java/com/devin/astonconnect/Journal/JournalEntry1Fragment.java
@@ -1,14 +1,19 @@
 package com.devin.astonconnect.Journal;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -19,33 +24,113 @@ import java.util.ArrayList;
 
 
 public class JournalEntry1Fragment extends Fragment {
+    private Spinner moodSpinner, moodLocationSpinner, moodIntensitySpinner;
+    private EditText customMood, customMoodLocation;
+    private Button selectDateButton, selectTimeButton;
 
-    private Spinner moodSpinner;
-    private EditText customMood;
-    private ArrayList<String> moodOptions = new ArrayList<>();
+    //final set of values from user's input
+    private String entryMood, entryLocation, entryIntensity, entryTime;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_journal_entry1, container, false);
         //custommood
-        customMood = view.findViewById(R.id.customMood);
 
-        //moodspinner
+        //Mood Spinner
         moodSpinner = view.findViewById(R.id.moodSpinner);
+        customMood = view.findViewById(R.id.customMood);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.mood_options, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         moodSpinner.setAdapter(adapter);
         moodSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String text = adapterView.getItemAtPosition(i).toString();
-                if(text.equals("Other")){
+                String selectedMood = adapterView.getItemAtPosition(i).toString();
+
+                if(selectedMood.equals("Other")){
                     customMood.setVisibility(View.VISIBLE);
+
+                    customMood.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+                        @Override
+                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+                        @Override
+                        public void afterTextChanged(Editable editable) {
+                            entryMood = customMood.getText().toString();
+                            Toast.makeText(getActivity(), entryMood, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
                 } else {
                     customMood.setVisibility(View.GONE);
+                    entryMood = selectedMood;
                 }
-                Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(getActivity(), entryMood, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //Mood Location
+        moodLocationSpinner = view.findViewById(R.id.moodLocationSpinner);
+        customMoodLocation = view.findViewById(R.id.customMoodLocation);
+        ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.mood_location, android.R.layout.simple_spinner_item);
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        moodLocationSpinner.setAdapter(locationAdapter);
+        moodLocationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedMoodLocation = adapterView.getItemAtPosition(i).toString();
+
+                if(selectedMoodLocation.equals("Other")){
+                    customMoodLocation.setVisibility(View.VISIBLE);
+
+                    customMoodLocation.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+                        @Override
+                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+                        @Override
+                        public void afterTextChanged(Editable editable) {
+                            entryLocation = customMoodLocation.getText().toString();
+                            Toast.makeText(getActivity(), entryLocation, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                } else {
+                    customMoodLocation.setVisibility(View.GONE);
+                    entryLocation = selectedMoodLocation;
+                }
+
+                Toast.makeText(getActivity(), entryLocation, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //Mood Intensity
+        moodIntensitySpinner = view.findViewById(R.id.moodIntensitySpinner);
+        ArrayAdapter<CharSequence> intensityAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.mood_intensity, android.R.layout.simple_spinner_item);
+        intensityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        moodIntensitySpinner.setAdapter(intensityAdapter);
+        moodIntensitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedIntensity = adapterView.getItemAtPosition(i).toString();
+                entryIntensity = selectedIntensity;
+                Toast.makeText(getActivity(), entryIntensity, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -56,4 +141,7 @@ public class JournalEntry1Fragment extends Fragment {
 
         return view;
     }
+
+    //Date time
+
 }
