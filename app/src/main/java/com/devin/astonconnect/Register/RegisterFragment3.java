111ef6e6f3d2b1c722b2261bfd9f28367add4b62
Devin S <36890301+shingad1@users.noreply.github.com>
2021-03-03 18:56:40 +0000

    Register fragment 3 UI updates

--- app/src/main/java/com/devin/astonconnect/Register/RegisterFragment3.java
+++ app/src/main/java/com/devin/astonconnect/Register/RegisterFragment3.java
@@ -8,7 +8,11 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.devin.astonconnect.R;
@@ -17,6 +21,8 @@ import com.devin.astonconnect.R;
 public class RegisterFragment3 extends Fragment {
 
     private RelativeLayout nextButtonLayout;
+    private EditText aboutMeText, customInterests;
+    private Spinner interestsSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +30,35 @@ public class RegisterFragment3 extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_register3, container, false);
 
         Bundle bundle = getArguments();
-        Toast.makeText(getActivity(), bundle.getString("imageUrl"), Toast.LENGTH_SHORT).show();
 
         nextButtonLayout = view.findViewById(R.id.nextButtonLayout);
+        interestsSpinner = view.findViewById(R.id.interestsSpinner);
+        aboutMeText      = view.findViewById(R.id.aboutMe_text);
+        customInterests  = view.findViewById(R.id.customInterests);
+
+        //Spinner selection
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.interests, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        interestsSpinner.setAdapter(adapter);
+        interestsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedInterest = adapterView.getItemAtPosition(i).toString();
+                if(selectedInterest.equals("Other")){
+                    customInterests.setVisibility(View.VISIBLE);
+                } else {
+                    customInterests.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
         nextButtonLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
