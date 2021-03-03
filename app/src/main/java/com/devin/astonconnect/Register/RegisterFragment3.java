9cdee1ec812ba6a8f632ededef7cfb90a66f71e6
Devin S <36890301+shingad1@users.noreply.github.com>
2021-03-03 13:55:19 +0000

    navigation

--- app/src/main/java/com/devin/astonconnect/Register/RegisterFragment3.java
+++ app/src/main/java/com/devin/astonconnect/Register/RegisterFragment3.java
@@ -3,20 +3,34 @@ package com.devin.astonconnect.Register;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.devin.astonconnect.R;
 
 
 public class RegisterFragment3 extends Fragment {
 
+    private RelativeLayout nextButtonLayout;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register3, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_register3, container, false);
+
+
+        nextButtonLayout = view.findViewById(R.id.nextButtonLayout);
+        nextButtonLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_registerFragment3_to_registerFragment4);
+            }
+        });
+
+        return view;
     }
 }
