62126e936cdfaaaf0d3c848ce444b63f1e02bdb6
Devin S <36890301+shingad1@users.noreply.github.com>
2021-03-07 12:17:08 +0000

    error handling

--- app/src/main/java/com/devin/astonconnect/LoginRegister/ForgotPasswordActivity.java
+++ app/src/main/java/com/devin/astonconnect/LoginRegister/ForgotPasswordActivity.java
@@ -49,14 +49,15 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String emailText = email_text.getText().toString();
                 Matcher matcher = pattern.matcher(emailText);
-
-                //if(TextUtils.isEmpty(emailText)){
-                //    Toast.makeText(ForgotPasswordActivity.this, "Please enter an email", Toast.LENGTH_SHORT).show();
-               // } else {
+                if (TextUtils.isEmpty(emailText)) {
+                    Toast.makeText(ForgotPasswordActivity.this, "Please enter an email", Toast.LENGTH_SHORT).show();
+                } else if (!matcher.matches()) {
+                    Toast.makeText(ForgotPasswordActivity.this, "Please enter an aston email", Toast.LENGTH_SHORT).show();
+                } else {
                     FirebaseAuth.getInstance().sendPasswordResetEmail(emailText).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
+                            if (task.isSuccessful()) {
                                 Toast.makeText(ForgotPasswordActivity.this, "Please check your email to reset your password", Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(ForgotPasswordActivity.this, "Something went wrong. Please try again later.", Toast.LENGTH_SHORT).show();
@@ -64,7 +65,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                         }
                     });
                 }
-          // }
+            }
         });
     }
 }
