52bbe9d6ebed79fbafbb32936b778d016239ac27
Devin S <36890301+shingad1@users.noreply.github.com>
2021-03-20 15:24:10 +0000

    transitions

--- app/src/main/java/com/devin/astonconnect/Onboarding/OnboardingFragment1.java
+++ app/src/main/java/com/devin/astonconnect/Onboarding/OnboardingFragment1.java
@@ -10,18 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.devin.astonconnect.R;
+import com.devin.astonconnect.SharedPreferencesManager;
 
 
 public class OnboardingFragment1 extends Fragment {
 
     private Button nextbutton;
+    private SharedPreferencesManager manager;
+    private Boolean isStaff;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.fragment_onboarding1, container, false);
+        //get is staff
+        manager = new SharedPreferencesManager(getActivity());
+        isStaff = manager.getisStaff();
+        Toast.makeText(getActivity(), "Is staff?: " + isStaff.toString(), Toast.LENGTH_SHORT).show();
 
         nextbutton = view.findViewById(R.id.nextButton);
         nextbutton.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +40,6 @@ public class OnboardingFragment1 extends Fragment {
                 navController.navigate(R.id.action_onboardingFragment1_to_onboardingFragment2);
             }
         });
-
         return view;
     }
 }
