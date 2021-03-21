7a0442a8ca0bbc4808d7663b7dd0c797d1112ce4
Devin S <devin_shingadia11@hotmail.com>
2021-03-21 23:17:27 +0000

    ui updates

--- app/src/main/java/com/devin/astonconnect/Onboarding/OnboardingFragment1.java
+++ app/src/main/java/com/devin/astonconnect/Onboarding/OnboardingFragment1.java
@@ -10,17 +10,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.devin.astonconnect.R;
 import com.devin.astonconnect.SharedPreferencesManager;
 
 
 public class OnboardingFragment1 extends Fragment {
 
-    private Button nextbutton;
+    private RelativeLayout nextbutton;
     private SharedPreferencesManager manager;
     private Boolean isStaff;
+    private LottieAnimationView lottieAnimation;
+    private TextView subText;
 
 
     @Override
@@ -31,6 +36,18 @@ public class OnboardingFragment1 extends Fragment {
         manager = new SharedPreferencesManager(getActivity());
         isStaff = manager.getisStaff();
         Toast.makeText(getActivity(), "Is staff?: " + isStaff.toString(), Toast.LENGTH_SHORT).show();
+        lottieAnimation = view.findViewById(R.id.lottieAnimation);
+        lottieAnimation.setSpeed(1f);
+        lottieAnimation.playAnimation();
+        
+        //text
+        subText = view.findViewById(R.id.subText);
+        if(isStaff){
+            subText.setText("provide CS Department updates to students in isolation");
+        } else {
+            subText.setText("recieve updates from fellow cs students and the cs department");
+        }
+
 
         nextbutton = view.findViewById(R.id.nextButton);
         nextbutton.setOnClickListener(new View.OnClickListener() {
